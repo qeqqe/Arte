fd6facc9814548df2c5e14cffa2a202029deef89
qeqqe <armansingh3172006@gmail.com>
2025-12-08 20:59:34 +0530

    Init, add starter api-gateway

---
+++ api-gateway/src/main/java/com/arte/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.arte.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
